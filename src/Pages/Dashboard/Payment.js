d89c383e4d0d1d97892aeb43b4248daaa6b64cd0
Yshahmpolash <iam@shahmpolash>
2022-05-25 17:50:35 +0600

    paymentGateway

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import usePayment from '../../hooks/usePayment';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L0iFOHYRbzBoepBIatnCVtApveRFaTmhw8Ijm5W6NFlFDvruSWcOFr50PLLcwz5U3UeEaMrTOhDMT6Jq8ZYxAIl00wfMhOesc');
 
 const Payment = () => {
     const { id } = useParams();
@@ -8,7 +13,11 @@ const Payment = () => {
     return (
         <div className='container text-center mt-5'>
             <h5>You are paying for: {order.itemname}</h5>
-            <h2>Payable amount: ${(order.price)*(order.quantity)} for {order.quantity} pics</h2>
+            <h2>Payable amount: ${(order.price) * (order.quantity)} for {order.quantity} pics</h2>
+            
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
